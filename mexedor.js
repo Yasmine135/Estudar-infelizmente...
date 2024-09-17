c3c77a0e76e54783e6237d8b4b2669d934ea2226
Yasmine135 <132566027+Yasmine135@users.noreply.github.com>
2024-09-17 11:33:15 -0300

    letconteiner

---
+++ mexedor.js
@@ -0,0 +1,18 @@
+function criaCartao(categoria, pergunta, resposta){
+let container = document.getElementById('container');
+let cartao = document.createElement('article');
+cartao.className = 'cartao';
+
+cartao.innerHTML = `
+<div class="cartao-conteudo">
+                <h3>${categoria}</h3>
+                <div class = "categoria-pergunta-resposta">
+                    <p>${pergunta}</p>
+                </div>
+                <div class = "categoria-pergunta-resposta">
+                    <p>${resposta}</p>
+                </div>
+            </div>`
+                
+     container.appendChild(cartao);
+}
