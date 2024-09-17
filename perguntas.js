85d1334ecd7cad92e7865bd2164fefa6298f8985
Yasmine135 <132566027+Yasmine135@users.noreply.github.com>
2024-09-17 10:46:53 -0300

    , vnlcçhohk,p~gdkt 0-dt98mr waç8ny

---
+++ perguntas.js
@@ -0,0 +1,100 @@
+criaCartao(
+    'Física',
+    'Fórmula de MUV?',
+    'v=v⁰+t.a')
+    
+    criaCartao(
+    'Física',
+    'Fórmula para encontrar o período:',
+    'T = 1 / F,')
+    
+    criaCartao(
+    'química',
+    'principais elementos da química orgânica?',
+    'C H O N P S')
+    
+    criaCartao(
+    'Química',
+    'tipos de isomeria',
+    'tautomeria /n  Isomeria de cadeia Isomeria de posição /n Isomeria de compensação/metameria /n Isomeria geometrica /n Isomeria óptica')
+    
+    criaCartao(
+    'Física',
+    'Fórmula de capacitores:',
+    'C = q/v')
+    
+    criaCartao(
+    'Física',
+    'o macete “fimose é um pênis mais uma pelisinha” refere-se a fórmula…',
+    'Foco=1/p+1/p¹')
+    
+    criaCartao(
+    'Física',
+    'o macete “fessor, quando ver bosta senta!” refere-se a fórmula…',
+    'F=q.v.Ɓ.sen')
+    
+    criaCartao(
+    'Fisica',
+    'Fórmula energia mecânica',
+    'Em = Ec + Epg')
+    
+    criaCartao(
+    'Química',
+    'Nomenclatura hidracidos',
+    'ácido + nome do elemento + ídrico')
+    
+    criaCartao(
+    'biologia',
+    'as plantas se classificam em…',
+    'algas verdes /n Briofitas /n Pteridofitas /n Giminospermas /n Angiospermas')
+    
+    criaCartao(
+    'Física',
+    'Fórmula de magnetismo',
+    'F=K.Q. q/d²')
+    
+    criaCartao(
+    'Matemática',
+    'Qual é a fórmula da área de um triângulo?',
+    'A = (base x altura) ÷ 2')
+    
+    criaCartao(
+    'Matemática',
+    'Fórmula Teorema de Pitágoras',
+    'a^2 + b^2 = c^2')
+    
+    criaCartao(
+    'Matemática ',
+    'Fórmula de bhaskara',
+    'x=-b +- raiz de b²-4ac/a.2')
+    
+    criaCartao(
+    'história',
+    'Qual foi o principal motivo das Grandes Navegações europeias no século XV?',
+    'Buscar novas rotas comerciais para as Índias e expandir territórios.')
+    
+    criaCartao(
+    'história',
+    'O que foi a Revolução Industrial?',
+    'Foi o processo de transformação econômica e social que começou no século XVIII, caracterizado pela mecanização da produção e o surgimento das fábricas.')
+    
+    criaCartao(
+    'Português',
+    'O que é um predicado?',
+    'O predicado é a parte da oração que contém o verbo e informa algo sobre o sujeito.')
+    
+    criaCartao(
+    'Português',
+    'O que é um substantivo abstrato?',
+    'Substantivo abstrato é aquele que nomeia ações, estados, qualidades ou sentimentos, como "amor", "felicidade" ou "esperança”')
+    
+    criaCartao(
+    'Português',
+    'O que é uma metáfora?',
+    'Metáfora é uma figura de linguagem que estabelece uma comparação implícita entre dois elementos, sem o uso de conectivos como "como" ou "tal qual".')
+    
+    criaCartao(
+    'Português',
+    'O que é uma metáfora?',
+    'Metáfora é uma figura de linguagem que estabelece uma comparação implícita entre dois elementos, sem o uso de conectivos como "como" ou "tal qual".')
+    
